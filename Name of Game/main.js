e56d272d318b646d3738b1f35b128f14686f8453
Grace Ho <grho@ucsc.edu>
2022-10-10 21:32:41 -0700

    movable bowling ball

--- Name of Game/main.js
+++ Name of Game/main.js
@@ -1,15 +1,51 @@
-title = "";
+title = "TITLE";
 
 description = `
+DESC
 `;
 
-characters = [];
+characters = [
+  `
+ llll
+ll l l
+lll ll
+llllll
+llllll
+ llll 
+`
+,
+`
+ ll
+ ll
+llll
+llll
+ ll 
+`
+];
 
-options = {};
+const G = {
+  WIDTH: 120,
+  HEIGHT: 100, 
+}
+
+options = {
+   //theme: "pixel",
+   viewSize: {x: G.WIDTH, y: G.HEIGHT},
+   //isPlayingBgm: true,
+   //isReplayEnabled: true,
+   //seed: 100
+};
 
 function update() {
-  if (!ticks) {
-  }
-}
+   if (!ticks) {
+   player = {
+      pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5)};
+   }
+
+   player.pos = vec(input.pos.x, input.pos.y);
+   player.pos.clamp(0, G.WIDTH, 90, 91);
+
+   char("a", player.pos);
+}  
 
 addEventListener("load", onLoad);
